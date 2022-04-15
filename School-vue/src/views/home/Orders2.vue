87b2fc57907dacf58364322bcd3bd0be9ca28861
“tanl1441” <tanl1441@fingard.com>
2022-04-15 16:10:00 +0800

    77测试

--- School-vue/src/views/home/Orders2.vue
+++ School-vue/src/views/home/Orders2.vue
@@ -1,5 +1,28 @@
 <template>
     <div class="Ad">
+        <!-- 搜索条件 -->
+        <el-row :gutter="20" class="goodsindex-queryInfo">
+            <!-- 商品名称搜索 -->
+            <el-col :xs="8" :sm="6" :md="6" :lg="4" :xl="4">
+                <el-input class="goodsindex-queryInfo-li" v-model="Order.name" clearable size="small"
+                          placeholder="请输入产品名称"></el-input>
+            </el-col>
+            <el-col :xs="8" :sm="6" :md="6" :lg="4" :xl="4">
+                <el-input class="goodsindex-queryInfo-li" v-model="Order.code" clearable size="small"
+                          placeholder="请输入订单编号"></el-input>
+            </el-col>
+
+            <el-col :xs="6" :sm="4" :md="3" :lg="2" :xl="2">
+                <el-button type="primary" @click="selectAdvance" class="goodsindex-queryInfo-li" size="small">多级搜索
+                </el-button>
+            </el-col>
+            <el-col :xs="6" :sm="4" :md="3" :lg="2" :xl="2">
+                <el-button type="danger" @click="clearAdvanced" class="goodsindex-queryInfo-li" size="small">重置
+                </el-button>
+            </el-col>
+
+        </el-row>
+        <br>
         <!-- 查询列表 -->
         <el-row class="Ad-list">
             <el-col :span="24">
@@ -43,7 +66,8 @@
                             label="状态"
                             width="170">
                         <template slot-scope="scope">
-                            <i style="font-size: 20px;color: red" v-if="scope.row.type == '下单了'" class="el-icon-question">待办</i>
+                            <i style="font-size: 20px;color: red" v-if="scope.row.type == '下单了'"
+                               class="el-icon-question">待办</i>
 
                             <i style="font-size: 20px; color: green" v-else class="el-icon-s-claim">已解决</i>
                         </template>
@@ -91,6 +115,7 @@
                 .post("/consumer/user/getAllUserMessage?userid=" + userId)
                 .then(response => {
                     this.dynamicList = response.data
+                    this.dynamicList2 = response.data
                     this.queryInfo.total = this.dynamicList.length
                 })
         },
@@ -98,7 +123,15 @@
         data() {
 
             return {
+                Order: {
+                    name: '',
+                    code: '',
+                },
+
                 dynamicList: [],
+                dynamicList2: [], //备用
+                dynamicList3: [], //备用
+
                 //搜索框分页
                 queryInfo: {
                     name: '',
@@ -108,6 +141,7 @@
                     pageSize: 10,
                     total: 0,
                 },
+
 
             }
         },
@@ -125,6 +159,62 @@
                     query: {id: id}
                 })
             },
+
+            selectAdvance() {
+                var j = 0
+                if (this.Order.code != '') {
+                    if (this.Order.name != '') {
+                        for (let i = 0; i < this.dynamicList2.length; i++) {
+                            if(this.dynamicList2[i].ordNumber == this.Order.code && this.dynamicList2[i].goodName == this.Order.name){
+                                this.dynamicList3[j++] = this.dynamicList2[i];
+                            }
+                        }
+                        if(j != 0){
+                            this.dynamicList = JSON.parse(JSON.stringify(this.dynamicList3));
+                            this.dynamicList3.length = 0
+                            j = 0;
+                        }else{
+                            this.$message.warning('未查询到数据')
+                        }
+                    }else{
+                        for (let i = 0; i < this.dynamicList2.length; i++) {
+                            if(this.dynamicList2[i].ordNumber == this.Order.code){
+                                this.dynamicList3[j++] = this.dynamicList2[i];
+                            }
+                        }
+                        if(j != 0){
+                            this.dynamicList = JSON.parse(JSON.stringify(this.dynamicList3));
+                            this.dynamicList3.length = 0
+                            j = 0;
+                        }else{
+                            this.$message.warning('未查询到数据')
+                        }
+                    }
+                } else if (this.Order.name != '') {
+                    for (let i = 0; i < this.dynamicList2.length; i++) {
+                        if(this.dynamicList2[i].goodName == this.Order.name){
+                            this.dynamicList3[j++] = this.dynamicList2[i];
+                        }
+                    }
+                    if(j != 0){
+                        this.dynamicList = JSON.parse(JSON.stringify(this.dynamicList3));
+                        this.dynamicList3.length = 0
+                        j = 0;
+                    }else{
+                        this.$message.warning('未查询到数据')
+                    }
+                }else{
+                    this.$message.error('查询条件未空')
+                }
+
+
+            },
+            clearAdvanced() {
+                this.Order.name = ''
+                this.Order.code = ''
+                this.dynamicList = JSON.parse(JSON.stringify(this.dynamicList2));
+            },
+
         }
     }
 </script>
